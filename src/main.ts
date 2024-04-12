bc548d98113efea17931ed41f505228f3db910ff
Pluto <3245531763@qq.com>
2024-04-12 11:07:09 +0800

    第一次

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import ElementPlus from 'element-plus';
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+const app = createApp(App);
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.mount('#app');
